2e5a41bd97d658658bdf08b7171728438dbac8e2
Gourab Mukherjee <aec.cse2024gourabmukherjee@gmail.com>
2026-01-08 16:58:32 +0530

    Add files via upload

---
+++ Request.js
@@ -0,0 +1,11 @@
+function Request(id,title,description,createdBy){
+    return{
+        id,
+        title,
+        description,
+        createdBy
+
+    };
+}
+
+module.exports = Request;
---
+++ User.js
@@ -0,0 +1,10 @@
+function User(id,name,email,password) {
+return {
+    id,
+    name,
+    email,
+    password
+  };
+}
+
+module.exports = User;
